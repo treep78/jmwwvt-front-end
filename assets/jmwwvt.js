16c948b5237aeedb2bf5edd01519c3a088e4376f
treep78 <treep78@gmail.com>
2017-01-20 09:10:50 -0500

    fix merge errors

--- assets/jmwwvt.js
+++ assets/jmwwvt.js
@@ -544,7 +544,7 @@ define('jmwwvt/initializers/data-adapter', ['exports', 'ember'], function (expor
   /*
     This initializer is here to keep backwards compatibility with code depending
     on the `data-adapter` initializer (before Ember Data was an addon).
-  
+
     Should be removed for Ember Data 3.x
   */
 
@@ -557,31 +557,31 @@ define('jmwwvt/initializers/data-adapter', ['exports', 'ember'], function (expor
 define('jmwwvt/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
 
   /*
-  
+
     This code initializes Ember-Data onto an Ember application.
-  
+
     If an Ember.js developer defines a subclass of DS.Store on their application,
     as `App.StoreService` (or via a module system that resolves to `service:store`)
     this code will automatically instantiate it and make it available on the
     router.
-  
+
     Additionally, after an application's controllers have been injected, they will
     each have the store made available to them.
-  
+
     For example, imagine an Ember.js application with the following classes:
-  
+
     App.StoreService = DS.Store.extend({
       adapter: 'custom'
     });
-  
+
     App.PostsController = Ember.Controller.extend({
       // ...
     });
-  
+
     When the application is initialized, `App.ApplicationStore` will automatically be
     instantiated, and the instance of `App.PostsController` will have its `store`
     property set to that instance.
-  
+
     Note that this code will only be run if the `ember-application` package is
     loaded. If Ember Data is being used in an environment other than a
     typical application (e.g., node.js where only `ember-runtime` is available),
@@ -694,7 +694,7 @@ define('jmwwvt/initializers/injectStore', ['exports', 'ember'], function (export
   /*
     This initializer is here to keep backwards compatibility with code depending
     on the `injectStore` initializer (before Ember Data was an addon).
-  
+
     Should be removed for Ember Data 3.x
   */
 
@@ -723,7 +723,7 @@ define('jmwwvt/initializers/store', ['exports', 'ember'], function (exports, _em
   /*
     This initializer is here to keep backwards compatibility with code depending
     on the `store` initializer (before Ember Data was an addon).
-  
+
     Should be removed for Ember Data 3.x
   */
 
@@ -752,7 +752,7 @@ define('jmwwvt/initializers/transforms', ['exports', 'ember'], function (exports
   /*
     This initializer is here to keep backwards compatibility with code depending
     on the `transforms` initializer (before Ember Data was an addon).
-  
+
     Should be removed for Ember Data 3.x
   */
 
@@ -825,10 +825,6 @@ define('jmwwvt/portfolio/route', ['exports', 'ember'], function (exports, _ember
     model: function model() {
       var categories = this.get('store').findAll('category');
       var images = this.get('store').findAll('portfolio-image');
-<<<<<<< HEAD
-      console.log(this.get('auth.isAuthenticated'));
-=======
->>>>>>> master
       var loggedIn = this.get('auth.isAuthenticated');
       return { 'categories': categories, 'images': images, 'loggedIn': loggedIn };
     },
@@ -998,11 +994,8 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-<<<<<<< HEAD
   require("jmwwvt/app")["default"].create({"name":"jmwwvt","version":"0.0.0+3eb6c102"});
-=======
-  require("jmwwvt/app")["default"].create({"name":"jmwwvt","version":"0.0.0+e4523027"});
->>>>>>> master
+
 }
 
 /* jshint ignore:end */
